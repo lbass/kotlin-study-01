8cb40d7a105c6c78ef4f53b4185885dd2e3e1446
xoxoms@toss.im <kdw@toss.im>
2020-08-13 15:54:11 +0900

    5장 완료

--- src/com/lbass/study/code05/lamda/T02_람다_with.kt
+++ src/com/lbass/study/code05/lamda/T02_람다_with.kt
@@ -12,18 +12,38 @@ fun alphabet() : String {
     return result.toString()
 }
 
-fun main(args: Array<String>) {
-    println(alphabet())
-    println(reAlphabet())
-}
-
-fun reAlphabet() : String {
+fun re01Alphabet() : String {
     val stringBuilder = StringBuilder()
     return with(stringBuilder) {
+        /**
+         * with 에서 첫번째 인자를 람다의 수신객체로 만든다.
+         * stringBuilder = 람다에서의 this
+         */
         for (letter in 'A'..'Z') {
             this.append(letter)
         }
         append("\nNow I know the alphabet!")
         toString()
     }
+    /**
+     * = with(stringBuilder, {....}) 같은 표현이지만 가독성이 떨어진다.
+     */
+}
+
+fun re02Alphabet() = with(StringBuilder()) {
+    /**
+     * 메소드 이름 충돌이 발생했을 경우 this 를 사용하면 수신객체를 가르킨다
+     */
+    // this == StringBuilder
+    for (letter in 'A'..'Z') {
+        this.append(letter)
+    }
+    this.append("\nNow I know the alphabet!")
+    toString()
+}
+
+fun main(args: Array<String>) {
+    println(alphabet())
+    println(re01Alphabet())
+    println(re02Alphabet())
 }
---
+++ src/com/lbass/study/code05/lamda/T03_람다_apply.kt
@@ -0,0 +1,38 @@
+package com.lbass.study.code05.lamda
+
+import java.lang.StringBuilder
+
+/**
+ * apply 함수는 객체의 인스턴스를 만들면서 즉시 프로퍼티 중 일부를 초기화해야 하는 경우에 유용하다.
+ */
+fun printAlphabet() = StringBuilder().apply {
+    for (letter in 'A'..'Z') {
+        this.append(letter)
+    }
+    this.append("\nTEST")
+}.toString()
+
+/**
+ * 참고용 안드로이드 코드
+ * 객체를 생성과 동시에 초기화
+ */
+/*
+fun createViewWithCustomAttributes(context: ConText) =
+    TextView(context).apply {
+        text = "Sample Text"
+        textSize = 20.0
+        setPadding(10, 0, 0, 0)
+    }
+*/
+
+fun printRe01Alphabet() = buildString {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append("\nTEST")
+}
+
+fun main (args: Array<String>) {
+    print(printAlphabet())
+    print(printRe01Alphabet())
+}
