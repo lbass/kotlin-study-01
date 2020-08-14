2a42b934cf3becea245a7693c3070949a48c44b0
lbass <done0929@gmail.com>
2020-08-14 16:07:05 +0900

    7장은 주로 관례를 사용한 메소드의 오버로딩의 내용이 많아서 간단하게 읽고 넘어간다. 관례를 따를 경우 내용을 잘 모르는 개발자의 경우 부담을 가질 수 있으므로 적극적으로 활용을 하는 것이 과연 좋을지에 대한 의문이 든다.

---
+++ src/com/lbass/study/code07/T02_단항연산자_오버로딩.kt
@@ -0,0 +1,30 @@
+package com.lbass.study.code07
+
+operator fun Point.unaryMinus(): Point {
+    return Point(-value)
+}
+
+// 증가연산자
+operator fun Point.inc(): Point {
+    return Point(value + 1)
+}
+
+operator fun Point.dec(): Point {
+    return Point(value - 1)
+}
+
+fun main(args: Array<String>) {
+    val p = Point(10)
+    println(-p)
+
+    // 증가연산자는 스스로 값이 증가하므로 var 를 사용해야 한다.
+    var p2 = Point(10)
+    ++p2
+    println(p2)
+    p2++
+    println(p2)
+    --p2
+    println(p2)
+    p2--
+    println(p2)
+}
